43e5c56814dc6956b948a63693dbd2a5799d21e4
com-wushuang <mybestchen@163.com>
2021-09-22 20:29:13 +0800

    希尔排序解析

--- sort/shell_test.go
+++ sort/shell_test.go
@@ -1,9 +1,26 @@
 package sort
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestAbs(t *testing.T) {
+var shellArry = []int{8, 1, 5, 7, 2, 3, 9, 6, 4}
 
+func TestShell(t *testing.T) {
+	Shell(shellArry)
+	fmt.Printf("%v", shellArry)
+}
+
+func Shell(a []int) {
+	length := len(a)
+	for inc := length / 2; inc > 0; inc = inc / 2 {
+		for i := inc; i < length; i++ {
+			for j := i - inc; j >= 0; j = j - inc {
+				if a[j] > a[i] {
+					a[j], a[i] = a[i], a[j]
+				}
+			}
+		}
+	}
 }
